fd002064e392222e8982d908348021c767985634
Atanu Mojumdar <91253222+AtanuMojumdar@users.noreply.github.com>
2023-09-08 22:16:12 +0530

    modified PrimeNumbers.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 //Prime Number
 public class PrimeNumber {
     public static void main(String[] args) {
-        System.out.println("Restart");
+//        System.out.println("Restart "+args[0]);
         Scanner obj = new Scanner(System.in);
         System.out.print("Enter value of a: ");
         int a = obj.nextInt();
