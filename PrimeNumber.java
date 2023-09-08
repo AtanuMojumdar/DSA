58274b3ed9da25a8828e032dfe312c788560b73b
Atanu Mojumdar <91253222+AtanuMojumdar@users.noreply.github.com>
2023-09-08 19:23:59 +0530

    Prime number algorithm (O(n)= square root of n)

---
+++ PrimeNumber.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        System.out.println("Restart");
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter value of a: ");
+        int a = obj.nextInt();
+        int b = a;
+        System.out.println("Square root of "+b+" is " +Math.sqrt(b));
+        isprimenumber(b);
+    }
+
+    public static void isprimenumber(int n){
+        boolean result = false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if(n%i == 0){
+                result = true;
+                if(Math.sqrt(n) == i){
+                    System.out.println(i);
+                }
+                else{
+                System.out.println(i+ " "+n/i);
+                }
+            }
+        }
+        if(!result){
+            System.out.println(n+" is a Prime Number");
+        }
+        else{
+            System.out.println(n+" is not a Prime Number");
+        }
+    }
+
+}
