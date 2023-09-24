994850017a9b4b7e31aa736ead537582c9de7afb
Atanu Mojumdar <91253222+AtanuMojumdar@users.noreply.github.com>
2023-09-24 19:14:23 +0530

    Ceiling BS

---
+++ BinarySearch/src/Ceiling.java
@@ -0,0 +1,23 @@
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,5};
+        System.out.println(arr[ceiling(arr,3)]);
+    }
+
+    private static int ceiling(int[] arr,int target) {
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]== target){
+                return mid;
+            } else if (arr[mid]<target) {
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        return start;
+    }
+}
